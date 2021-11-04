34c67a9c4f088231e0e5a798eea2f469070c53d3
Muxtar0 <muxtarakberov2005@gmail.com>
2021-11-04 20:25:13 +0400

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,17 @@ for(let i = 0 ; i < FaqContent.length ; i++){
         FaqContent[i].classList.toggle('active')
     })
 }
+
+const HamburgerMenuOpenBtn = document.querySelector('.HamburgerMenuOpen .hamburger');
+const MobileOpenMenu = document.querySelector('.MobileOpenMenu');
+const header = document.querySelector('.header');
+const TopSection = document.querySelector('#TopSection')
+HamburgerMenuOpenBtn.addEventListener('click' , () => {
+    HamburgerMenuOpenBtn.classList.toggle('is-active');
+    MobileOpenMenu.classList.toggle('active');
+    setTimeout(() => {
+        MobileOpenMenu.classList.toggle('active2');
+    },500)
+    header.classList.toggle('active');
+    TopSection.classList.toggle('active')
+})
